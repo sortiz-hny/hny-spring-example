8e621b9f8216e9bae811ef748381ed4db7733985
sortiz-hny <stephanie@honeycomb.io>
2021-06-09 11:10:16 -0400

    WIP: Use honeycomb-opentelemetry-java for otel metrics

--- src/main/java/com/honeycomb/demo/hnyexample/controller/BeerAPIService.java
+++ src/main/java/com/honeycomb/demo/hnyexample/controller/BeerAPIService.java
@@ -10,50 +10,52 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.honeycomb.demo.hnyexample.model.Beer;
 
-import io.honeycomb.beeline.spring.beans.aspects.ChildSpan;
-import io.honeycomb.beeline.tracing.Beeline;
-import io.honeycomb.beeline.tracing.Span;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
 
 @Service
 public class BeerAPIService {
 	
 	private ObjectMapper mapper;
 	private final RestTemplate restTemplate;
-	private final Beeline beeline;
 	
 	private static final String BEER_ENDPOINT = "https://random-data-api.com/api/beer/random_beer";
 	
 	@Autowired
-	public BeerAPIService(RestTemplate restTemplate, Beeline beeline) {
+	public BeerAPIService(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 		mapper = new ObjectMapper();
-		this.beeline = beeline;
 	}
 	
-	@ChildSpan("getBeer")
 	public Beer getBeer() throws JsonMappingException, JsonProcessingException {
-		Span childSpan = beeline.getActiveSpan();
-		childSpan.addField("external-rest-endpoint", BEER_ENDPOINT);
-		
-		String response = restTemplate
-				.exchange(BEER_ENDPOINT, HttpMethod.GET, null, String.class)
-				.getBody();
-		
-		Beer beer = mapper.readValue(response, Beer.class);
-		if (beer != null) {
-			String percentStr = beer.getAlcohol().substring(0, beer.getAlcohol().length()-1);
-			double percent = Double.parseDouble(percentStr);
+		Span span = null;
+		Beer beer = null;
+		try {
+			span = Span.current();
+			String response = restTemplate
+					.exchange(BEER_ENDPOINT, HttpMethod.GET, null, String.class)
+					.getBody();
 			
-			String ibuStr = beer.getIbu().split(" ")[0];
-			int ibu = Integer.parseInt(ibuStr);
-			
-			childSpan.addField("beer.id", beer.getId());
-			childSpan.addField("beer.name", beer.getName());
-			childSpan.addField("beer.style", beer.getStyle());
-			childSpan.addField("beer.brand", beer.getBrand());
-			childSpan.addField("beer.ibu", ibu);
-			childSpan.addField("beer.alchol_percent", percent);
-			childSpan.addField("beer.misc", beer.getHop() + "\n " + beer.getMalts() + " \n" + beer.getYeast());
+			beer = mapper.readValue(response, Beer.class);
+			if (beer != null) {
+				String percentStr = beer.getAlcohol().substring(0, beer.getAlcohol().length()-1);
+				double percent = Double.parseDouble(percentStr);
+				
+				String ibuStr = beer.getIbu().split(" ")[0];
+				int ibu = Integer.parseInt(ibuStr);
+				
+				span.setAttribute("beer.id", beer.getId());
+				span.setAttribute("beer.name", beer.getName());
+				span.setAttribute("beer.style", beer.getStyle());
+				span.setAttribute("beer.brand", beer.getBrand());
+				span.setAttribute("beer.ibu", ibu);
+				span.setAttribute("beer.alcohol_percent", percent);
+				span.setAttribute("beer.misc", beer.getHop() + "\n " + beer.getMalts() + " \n" + beer.getYeast());
+			}
+		} finally {
+			if (span != null) {
+				span.end();
+			}
 		}
 		return beer;
 	}
